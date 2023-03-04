b82949231d16a271e32bde432012cdc0cc2ae480
blockodyssey-uhui <reah@blockodyssey.io>
2023-03-04 19:24:00 +0900

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+        <>
+            <title>ChatGPT Clone</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    );
+}
